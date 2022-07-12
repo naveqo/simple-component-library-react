c41f7a5359bd1f14f45e579257b9be7f53ba8dd0
naveqo <422855+naveqo@users.noreply.github.com>
2022-07-12 22:38:12 +0900

    feat: add Button component

---
+++ src/Button/Button.tsx
@@ -0,0 +1,10 @@
+import style from './Button.module.css'
+
+type Props = {
+  size?: 'small' | 'medium';
+  children: React.ReactNode
+}
+
+export const Button = (props: Props) => {
+  return <button className={style.button}>{props.children}</button>
+}
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      'jsxRuntime': 'classic'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (ext) => `index.${ext}.js`,
+      // for UMD name: 'GlobalName'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    },
+    target: 'esnext',
+    sourcemap: true
+  }
+})
